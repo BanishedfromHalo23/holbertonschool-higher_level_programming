5537e77a1fff999c84099a33a8c2bafee1b5cfdd
BanishedfromHalo23 <williamls1031@gmail.com>
2025-09-21 20:26:10 -0400

    task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines an empty class Square."""
+
+
+class Square:
+    """Represents a square.
+
+    This class is currently empty and serves as a placeholder
+    for future implementation of square-related functionality.
+    """
+    pass
